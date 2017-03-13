1eafb57d0fe72199352b4cf7b36e2bf6eabe281e
Scavezze, Nathan <NScavezze@cobank.com>
2017-03-13 17:21:15 -0600

    added icon fonts

--- app/modules/common/common.module.ts
+++ app/modules/common/common.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
+import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
+
 
 import { MenuComponent } from "./components/menu/menu.component";
 
 @NgModule({
     imports: [
         NativeScriptModule,
-        NativeScriptUISideDrawerModule
+        NativeScriptUISideDrawerModule,
+        TNSFontIconModule.forRoot({
+            'fa': './assets/font-awesome.css'
+        })
     ],
     exports: [
         MenuComponent
